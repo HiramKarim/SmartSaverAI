a99d84a8bb5797027c22fad664a2622743fd1c4e
Hiram Castro <hiram2027@gmail.com>
2024-03-28 19:46:49 -0700

    changing main project as workspace to work with multiple modules.
    Adding persistence module

---
+++ PersistenceModule/PersistenceModule/PersistenceModule.h
@@ -0,0 +1,18 @@
+//
+//  PersistenceModule.h
+//  PersistenceModule
+//
+//  Created by Hiram Castro on 28/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PersistenceModule.
+FOUNDATION_EXPORT double PersistenceModuleVersionNumber;
+
+//! Project version string for PersistenceModule.
+FOUNDATION_EXPORT const unsigned char PersistenceModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceModule/PublicHeader.h>
+
+
---
+++ SavingsApp/SavingsApp/SavingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingsAppApp.swift
+//  SavingsApp
+//
+//  Created by Hiram Castro on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SavingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
