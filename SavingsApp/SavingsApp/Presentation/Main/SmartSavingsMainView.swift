0c649447e64af4346894751f9869e262acb19c36
Hiram Castro <hiramcastrodeveloper@gmail.com>
2024-03-29 16:13:14 -0700

    refactoring code

--- SavingsApp/SavingsApp/Presentation/Main/SmartSavingsMainView.swift
+++ SavingsApp/SavingsApp/Presentation/Main/SmartSavingsMainView.swift
@@ -17,25 +17,59 @@ struct SmartSavingsMainView: View {
                 Divider()
                 ScrollView {
                     
-                    ViewThatFits(in: .vertical) {
-                        RoundedRectangle(cornerRadius: 16)
-                        .fill(Color.blue)
-                        .overlay(
-                            VStack(spacing: 20) {
-                                Text("Total Balance")
+                    VStack {
+                        ViewThatFits(in: .vertical) {
+                            RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.blue)
+                            .overlay(
+                                VStack(spacing: 20) {
+                                    Text("Total Balance")
+                                        .font(.system(size: 30))
+                                    Text("$2,771")
+                                        .font(.system(size: 70))
+                                }
+                                .foregroundColor(Color.white)
+                                .bold()
+                            )
+                            .frame(width: 350, height: 200)
+                            .padding()
+                        }
+                        
+                        HStack {
+                            ViewThatFits(in: .vertical) {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .fill(Color.purple)
+                                .overlay(
+                                    VStack(spacing: 10) {
+                                        Text("Income")
+                                        Text("$2,771")
+                                    }
+                                    .foregroundColor(Color.white)
+                                    .bold()
                                     .font(.system(size: 30))
-                                Text("$2,771")
-                                    .font(.system(size: 70))
+                                )
+                                .frame(width: 150, height: 130)
+                                .padding()
                             }
-                            .padding(.top, 10)
-                            .foregroundColor(Color.white)
-                            .bold()
-                        )
-                        .frame(width: 350, height: 200)
-                        .padding()
+                            
+                            ViewThatFits(in: .vertical) {
+                                RoundedRectangle(cornerRadius: 16)
+                                    .fill(Color.orange)
+                                .overlay(
+                                    VStack(spacing: 10) {
+                                        Text("Expense")
+                                        Text("$2,771")
+                                    }
+                                    .foregroundColor(Color.white)
+                                    .bold()
+                                    .font(.system(size: 30))
+
+                                )
+                                .frame(width: 150, height: 130)
+                                .padding()
+                            }
+                        }
                     }
-                    
-                    
                 }
             }
             .navigationTitle("Personal Finance")
